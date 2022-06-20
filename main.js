1d82b599985b5943667ed8745a4a1d4c400bfa78
Vito Farenga <vitofarenga@libero.it>
2022-06-20 17:13:43 +0200

    inizio java

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-images = [
+ = [
     'https://cdn.photographycourse.net/wp-content/uploads/2022/04/Portrait-vs-Landscape-Featured-Image-3.jpg',
     'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6.jpg',
     'https://cdn.photographycourse.net/wp-content/uploads/2014/11/Landscape-Photography-steps.jpg',
@@ -6,3 +6,10 @@ images = [
     'https://iso.500px.com/wp-content/uploads/2021/02/Torres-Del-Paine-Sunset-By-Paul-Reiffer-2-1500x1000.jpg',
     'https://mymodernmet.com/wp/wp-content/uploads/2020/02/Landscape-Photographer-of-the-Year-Sander-Grefte.jpg'
     ];
+    
+
+    const imgsCont = document.quesrySelector('.container #big #cont-2');
+    const imgList = imgsCont.children;
+    
+    const btnNext = document.getElementById('next');
+    const btnPrev = document.getElementById('prev');
