64137e1308c37c2ae4beb8000396c34ee7e79953
Vito Farenga <vitofarenga@libero.it>
2022-06-20 17:29:29 +0200

    lo skip funziona

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
- = [
+  [
     'https://cdn.photographycourse.net/wp-content/uploads/2022/04/Portrait-vs-Landscape-Featured-Image-3.jpg',
     'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6.jpg',
     'https://cdn.photographycourse.net/wp-content/uploads/2014/11/Landscape-Photography-steps.jpg',
@@ -7,9 +7,16 @@
     'https://mymodernmet.com/wp/wp-content/uploads/2020/02/Landscape-Photographer-of-the-Year-Sander-Grefte.jpg'
     ];
     
-
-    const imgsCont = document.quesrySelector('.container #big #cont-2');
+    let activeElementIndex = 1;
+    const imgsCont = document.querySelector('.container #big #cont-2');
     const imgList = imgsCont.children;
+    imgList[activeElementIndex].classList.add('active')
     
     const btnNext = document.getElementById('next');
     const btnPrev = document.getElementById('prev');
+
+    btnNext.addEventListener('click', function(){
+    imgList[activeElementIndex].classList.remove('active');
+    activeElementIndex++;
+    imgList[activeElementIndex].classList.add('active');
+    })
